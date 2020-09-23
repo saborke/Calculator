07835da4b0ca72ea54710f4f625dfa2b20e4e21c
Sukumar <Sukumar@DESKTOP-EGO2HU9>
2020-09-23 11:22:59 +0530

    First Commit

---
+++ Calculator/src/com/suku/Calculator.java
@@ -0,0 +1,12 @@
+package com.suku;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Operations op=new Operations();
+		System.out.println("Result is : "+op.add(5, 7));
+
+	}
+
+}
---
+++ Calculator/src/com/suku/Operations.java
@@ -0,0 +1,14 @@
+package com.suku;
+
+public class Operations {
+	public int add(int a,int b){
+		return a+b;
+	}
+
+	public int mul(int a,int b){
+		return a*b;
+	}
+	public int sub(int a,int b){
+		return a-b;
+	}
+}
